eb31cc36c88c4047aefef56112366be1a0c7bbc9
Jordan Cooper <jordan@cooperplanet.com>
2018-02-22 07:21:08 -0700

    Fix authentication issue with postgres

--- server/server.js
+++ server/server.js
@@ -4,8 +4,7 @@ const { graphql } = require('graphql')
 const testSchema = require('../db/schema')
 const graphqlHTTP = require('express-graphql')
 const pg = require('pg')
-const pgConfig = config.nodeEnv
-const pgPool = pg.Pool(pgConfig)
+const pgPool = new pg.Pool(config.pg)
 
 app.listen(config.port, () => {
   console.log(`Listening on port ${config.port}...`)
